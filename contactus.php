04f4819f3588517eac80d9c73ad6a422b4f07027
Sona Elizebeth Shaji <56604740+Sonaelizebeth@users.noreply.github.com>
2022-07-23 12:24:42 +0530

    Update contactus.php

--- contactus.php
+++ contactus.php
@@ -3,9 +3,9 @@
     $connection = mysqli_connect('localhost','root');
     mysqli_select_db($connection,"protrack");
 
-    $user = $_get['user'];
-    $email = $_get['email'];
-    $message = $_get['message'];
+    $user = $_POST['user'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
     $query = "INSERT INTO 'userinfo'('user','email','message') VALUES ('$user','$email','$message')";
 
     mysqli_query($connection,$query);
